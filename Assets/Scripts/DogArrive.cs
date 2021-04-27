91f03887b57ef2427d5aaf8163ae7ee7e230d46e
Jammie506 <37370214+Jammie506@users.noreply.github.com>
2021-04-27 16:13:44 +0100

    End

--- Assets/Scripts/DogArrive.cs
+++ Assets/Scripts/DogArrive.cs
@@ -4,16 +4,23 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 
-public class Arrive : SteeringBehaviour
+public class DogArrive: SteeringBehaviour
 {
     public Vector3 targetPosition = Vector3.zero;
     public float slowingDistance = 15.0f;
 
     public GameObject targetGameObject = null;
 
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine(transform.position, transform.position + force * 100);
+    }
+
     public override Vector3 Calculate()
     {
-        return boid.ArriveForce(targetPosition, slowingDistance);
+        Vector3 force = boid.ArriveForce(targetPosition, slowingDistance);
+        return force;
     }
 
     public void Update()
